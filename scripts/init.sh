459b49a46d7f8eb27026be6889b5392e4434c8de
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-06-09 21:45:17 +0900

    change action

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # we need binaryen as well, which can be installed via `sudo apt install binaryen` or `brew install binaryen`
-# update local toolchain
-rustup update
+# set local toolchain
+rustup show
 
 # install ink cli
 cargo install --force cargo-contract

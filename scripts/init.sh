64a46235d3583d01f7a18ff4aed7085f6fb29e24
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-06-09 22:32:14 +0900

    changed target

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # we need binaryen as well, which can be installed via `sudo apt install binaryen` or `brew install binaryen`
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
 # set local toolchain
 rustup show
 

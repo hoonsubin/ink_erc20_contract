bbb3428671f0988e61f8bd6ff09db4cae75c5ee0
Hoon Kim <mail@hoonkim.me>
2021-06-10 00:54:40 +0900

    add project CI (#1)
    * add toolchain config file
    
    * remove toml extension for toolchain config
    
    * add toolchain update
    
    * remove toolchain action input
    
    * change action
    
    * fix action run
    
    * Update pr-checks.yml
    
    * Update pr-checks.yml
    
    * add manual toolchain profile
    
    * install default profile
    
    * add rls component
    
    * changed target
    
    * changed toolchain target
    
    * set date
    
    * Update init.sh
    
    * Update pr-checks.yml
    
    * change channel
    
    * add binaryen
    
    * add sudo
    
    * add install binaryen package
    
    * fix package fetch command
    
    * Update pr-checks.yml
    
    * separate jobs
    
    * add upload contract action (test)
    
    * add glob pattern for contract
    
    * change contract path
    
    * Update upload-contract.yml

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# install tool chains
 # we need binaryen as well, which can be installed via `sudo apt install binaryen` or `brew install binaryen`
-rustup component add rust-src --toolchain nightly
-rustup target add wasm32-unknown-unknown --toolchain stable
+rustup target add wasm32-unknown-unknown --toolchain nightly
 
 # install ink cli
 cargo install --force cargo-contract
+
+rustup update
+# show local toolchain
+rustup show

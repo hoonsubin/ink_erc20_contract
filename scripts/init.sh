2df55f01a68f31f81a49bf641fa130f0539d0672
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-06-09 22:42:30 +0900

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,8 +3,9 @@
 # we need binaryen as well, which can be installed via `sudo apt install binaryen` or `brew install binaryen`
 rustup target add wasm32-unknown-unknown --toolchain nightly
 
-# set local toolchain
-rustup show
-
 # install ink cli
 cargo install --force cargo-contract
+
+rustup update
+# show local toolchain
+rustup show
